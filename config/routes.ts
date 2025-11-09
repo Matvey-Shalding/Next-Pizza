2ce843f694b1eb6f76c2b20c73b75a1f53c698c3
Mathew Shaldin <mathew.shaldin@gmail.com>
2025-11-09 13:31:22 +0300

    feat(api): create API client with Axios instance and route definitions

---
+++ config/routes.ts
@@ -0,0 +1,5 @@
+export const API_ROUTES = {
+  PRODUCTS_SEARCH: '/products/search',
+  PRODUCTS_DEFAULT: '/products/default',
+  INGREDIENTS: '/ingredients'
+} as const
---
+++ services/instance.ts
@@ -0,0 +1,7 @@
+// basic axios config
+
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+	baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
